9c4cfc6bfe70aab9bb06a7661f68dfbbdada603f
David Lemvigh <dlemvigh@gmail.com>
2025-01-18 20:38:07 +0100

    Fix a11y issue on font switcher

--- src/components/inputs/FontSwitcher.tsx
+++ src/components/inputs/FontSwitcher.tsx
@@ -6,30 +6,37 @@ type Font = "sans-serif" | "serif" | "mono";
 
 export function FontSwitcher() {
     const { font, setFont } = useUIContext();
-    const [isOpen, setIsOpen] = useState(true);
+    const [isOpen, setIsOpen] = useState(false);
     
     const toggleOpen = useCallback(() => {
         setIsOpen((prev) => !prev);
     }, [])
 
-    const onBlurOutside = useCallback((e: FocusEvent<HTMLButtonElement>) => {
+    const onOptionClick = useCallback((font: Font) => {
+        setFont(font);
+        setIsOpen(false);
+    }, [])
+
+    const onBlurOutside = useCallback((e: FocusEvent<HTMLElement>) => {
         if (!e.currentTarget.contains(e.relatedTarget as Node)) {
             setIsOpen(false);
         }
     }, [])
 
     return (
-        <button className={classes["font-switcher"]} onClick={toggleOpen} onBlur={onBlurOutside}>
-            <FontOption font={font} />
-            <img src="/images/icon-arrow-down.svg" className={`${classes.chevron}`} />
+        <div className={classes["font-switcher-container"]} onBlur={onBlurOutside}>
+            <button className={classes["font-switcher"]} onClick={toggleOpen}>
+                <FontOption font={font} />
+                <img src="/images/icon-arrow-down.svg" className={`${classes.chevron}`} />
+            </button>
             {isOpen && (
                 <div className={classes["dropdown"]}>
-                    <button className={classes["option"]} onClick={() => setFont("sans-serif")}><FontOption font="sans-serif" /></button>
-                    <button className={classes["option"]} onClick={() => setFont("serif")}><FontOption font="serif" /></button>
-                    <button className={classes["option"]} onClick={() => setFont("mono")}><FontOption font="mono" /></button>
+                    <button className={classes["option"]} onClick={() => onOptionClick("sans-serif")}><FontOption font="sans-serif" /></button>
+                    <button className={classes["option"]} onClick={() => onOptionClick("serif")}><FontOption font="serif" /></button>
+                    <button className={classes["option"]} onClick={() => onOptionClick("mono")}><FontOption font="mono" /></button>
                 </div>
             )}
-        </button>
+        </div>
     );   
 }
 
