49b80af0068d24f6ce32a9c01632dfcbe40c3250
David Lemvigh <dlemvigh@gmail.com>
2025-01-18 11:39:06 +0100

    DarkModeSwitcher styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import classes from "./Header.module.css"
+import { DarkModeSwitcher } from "./inputs/DarkModeSwitcher"
 
 export function Header() {
     return (
         <header className={classes.header}>
             <img className={classes.logo} src="/images/logo.svg" />
-            <span>
-                controls
-            </span>
+            <div>
+                <DarkModeSwitcher />
+            </div>
         </header>
     )
 }
---
+++ src/components/inputs/DarkModeSwitcher.tsx
@@ -0,0 +1,14 @@
+import classes from "./DarkModeSwitcher.module.css"
+
+export function DarkModeSwitcher() {
+    return (
+        <div className={classes["dark-mode-switcher"]}>
+            <label className={classes["dark-mode-label"]}>
+                <input type="checkbox" name="dark-mode" className={classes["dark-mode-input"]} />
+                <div className={classes["dark-mode-knob"]}></div>
+            </label>
+            
+            <svg className={classes["dark-mode-img"]} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M1 10.449a10.544 10.544 0 0 0 19.993 4.686C11.544 15.135 6.858 10.448 6.858 1A10.545 10.545 0 0 0 1 10.449Z"/></svg>
+        </div>
+    )
+}
